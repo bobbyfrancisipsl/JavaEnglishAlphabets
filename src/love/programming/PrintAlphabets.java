5cc406d5bf26f461db38d3c55a094017952d9d1d
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-03-06 18:42:23 +0000

    JavaEnglishAlphabets

---
+++ src/love/programming/PrintAlphabets.java
@@ -0,0 +1,18 @@
+package love.programming;
+
+public class PrintAlphabets {
+
+    static char ch,ch1,ch2,ch3;
+    public static void main(String[] args) {
+        ch = 'a';
+        ch1 = 'b';
+        ch2 = 'c';
+        ch3= 'd';
+        System.out.println(ch);
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+
+
+    }
+}
